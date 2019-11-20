32f600db54b6ec05a41c208c0dfa17351fcfa010
George Bell <george.bell@backslash.build>
2019-11-20 18:05:45 +0000

    lols

--- yodaquotes/src/App.js
+++ yodaquotes/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import AppWrapper from "./AppWrapper";
+import Quote from "./Quote";
 import "./App.css";
 
 function App() {
   return (
     <AppWrapper>
-      <div className="App">
+      <div className="App" style={{ padding: "0" }}>
         <h1>yoda app lol</h1>
         <img src="yodaswag.png" />
+        <Quote>This is where the quote will be</Quote>
       </div>
     </AppWrapper>
   );
