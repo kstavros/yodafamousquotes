f41fc1508577e548343b5a15e7bae6165e9eaf26
Andrei-Nicolae Trandafir <andrei.trandafir30@gmail.com>
2019-11-20 18:06:25 +0000

    Translate yoda, add

--- yodaquotes/src/App.js
+++ yodaquotes/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { yodaTranslate } from "./calls";
 
 function App() {
   return (
     <div className="App">
       <h1>yoda app lol</h1>
       <img src="yodaswag.png" />
+      <button onClick={async () => console.log(await yodaTranslate("You should translate this."))}>Click me</button>
     </div>
   );
 }
