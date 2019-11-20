72e907fd393ac8f6cfbe36ab84951b40d7b972bc
Andrei-Nicolae Trandafir <andrei.trandafir30@gmail.com>
2019-11-20 18:57:45 +0000

    finish

--- yodaquotes/src/App.js
+++ yodaquotes/src/App.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppWrapper from "./AppWrapper";
 import Quote from "./Quote";
 import Yoda from "./Yoda";
 import "./App.css";
 import Speak from "./components/Speak";
-import { yodaTranslate } from "./calls";
+import { yodaTranslate, getQuiz } from "./calls";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState({ quote: "loading", correct: "loading" });
+
+  useEffect(async () => {
+    const result = await getQuiz();
+    setValue(result);
+  }, []);
   return (
     <AppWrapper>
       <div className="App" style={{ padding: "0" }}>
-        <h1>yoda app lol</h1>
-        <img src="yodaswag.png" />
         <h1>Yoda Brainz</h1>
         <Yoda />
-        <Speak>This is where the quote will be</Speak>
+        <Speak>{!!value.quote ? value.quote : "loading"}</Speak>
+        <h2>{!!value.quote ? "Author: " + value.correct : "loading"}</h2>
+        <button onClick={async () => setValue(await getQuiz())}> Try Again</button>
       </div>
     </AppWrapper>
   );
